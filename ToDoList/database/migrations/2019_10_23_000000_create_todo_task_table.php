d1a3bf0038afd84ba4f48592e87518083f1e5991
Guilherme Mattioli <djmattioli@gmail.com>
2019-10-23 22:30:11 -0300

    GULP: melhoria da opção de install e adicionadas novas tasks (clean e reinstall) + Migrations: criação da tabela para manter a lista de tarefas

---
+++ ToDoList/database/migrations/2019_10_23_000000_create_todo_task_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToDoTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_task', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->enum('status', ['todo', 'done', 'deleted'])->default('todo')->index();
+            $table->timestamps();
+
+            $table->index(['user_id', 'status']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_task');
+    }
+}
--- ToDoList/gulpfile.js
+++ ToDoList/gulpfile.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
 // Load plugins
-const del = require("del");
-const gulp = require("gulp");
+const del = require('del');
+const gulp = require('gulp');
 const shell = require('gulp-shell');
 
 // show all tasks
@@ -15,22 +15,32 @@ function help() {
 function install() {
     return gulp
         .src('.')
-        .pipe(
-            shell(['composer install']),
-            shell(['touch database/database.sqlite']),
-            shell(['php artisan migrate'])
-        );
+        .pipe(shell([
+            'composer install',
+            'touch database/database.sqlite',
+            'php artisan migrate'
+        ]));
+}
+
+function clean() {
+    return del([
+        './database/database.sqlite',
+        './vendor'
+    ]);
 }
 
 function serve() {
     return gulp
         .src('.')
-        .pipe(
-            shell(['php artisan serve'])
-        );
+        .pipe(shell(['php artisan serve']));
 }
+
+// Define complex tasks
+const reinstall = gulp.series(clean, install);
 
 // Export tasks
 exports.help = help;
 exports.install = install;
+exports.clean = clean;
 exports.serve = serve;
+exports.reinstall = reinstall;
