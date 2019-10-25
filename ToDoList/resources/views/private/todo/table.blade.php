5b3cc941e817e433098c66aa548c8ae35d3b340c
Guilherme Mattioli <djmattioli@gmail.com>
2019-10-25 17:46:52 -0300

    alterada view padrão para exibição da table de tarefas para listagem completa e paginada, com campos ajustados e validação de tabela vazia

--- ToDoList/resources/views/private/todo/table.blade.php
+++ ToDoList/resources/views/private/todo/table.blade.php
@@ -1,20 +1,40 @@
-<table class="table" id="todo-table">
-    <thead class="thead-blue">
+<thead class="thead-blue">
+<tr>
+    {{-- <th scope="col">#</th>--}}
+    <th scope="col">Status</th>
+    <th scope="col">Description</th>
+    <th scope="col" width="20px"></th>
+</tr>
+</thead>
+<tbody>
+@forelse ($tasks as $task)
     <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <td colspan="2" class="p-1" task="{{$task['id']}}">
+            {{-- <td> {{$task['id']}} </td>--}}
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">
+                        <input type="checkbox" name="status"
+                            {{($task['status'] === 'done') ? 'checked="checked"' : ''}}>
+                    </div>
+                </div>
+                <input type="text" name="name" class="form-control" value="{{$task['name']}}">
+            </div>
+            {{-- {{$task['status']}}--}}
+        </td>
+        <td task="{{$task['id']}}">
+            <a href="#" onclick="return window.todo.del({{$task['id']}});">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </td>
     </tr>
-    </thead>
-    <tbody>
-    @foreach($tasks as $task)
-        <tr>
-            <td> {{$task['id']}} </td>
-            <td> {{$task['title']}} </td>
-            <td> {{$task['comment']}} </td>
-        </tr>
-    @endforeach
-    </tbody>
-    <caption class="text-right"> {{$tasks->links()}} </caption>
-</table>
+@empty
+    <tr>
+        <td colspan="3" class="p-1 text-center">
+            No tasks have been added yet.
+            <hr class="m-0">
+        </td>
+    </tr>
+@endforelse
+</tbody>
+<caption class="text-right"> {{$tasks->links()}} </caption>
