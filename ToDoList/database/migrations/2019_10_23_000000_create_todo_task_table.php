a6b8e2cb59b65a480ab9b54d9b02d036895b2037
Guilherme Mattioli <djmattioli@gmail.com>
2019-10-25 12:39:07 -0300

    create table atualizada para utilizar os status definidos na model para definição do ENUM de status

--- ToDoList/database/migrations/2019_10_23_000000_create_todo_task_table.php
+++ ToDoList/database/migrations/2019_10_23_000000_create_todo_task_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Http\Models\ToDoTask;
 
 class CreateToDoTaskTable extends Migration
 {
@@ -18,7 +19,7 @@ class CreateToDoTaskTable extends Migration
             $table->unsignedBigInteger('user_id')->index();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
-            $table->enum('status', ['todo', 'done', 'deleted'])->default('todo')->index();
+            $table->enum('status', ToDoTask::getAcceptedStatus())->default('todo')->index();
             $table->timestamps();
 
             $table->index(['user_id', 'status']);
