76e153e1e6e7ca2d9fae1b489038ef3f2240265d
Guilherme Mattioli <djmattioli@gmail.com>
2019-10-25 17:50:03 -0300

    incluido token CSRF e adicionada área para exibição de mensagens

--- ToDoList/resources/views/private/todo/listar.blade.php
+++ ToDoList/resources/views/private/todo/listar.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     {{-- <h1 class="h3 mb-4 text-gray-800">Tasks</h1> --}}
 
-    <!-- DataTales Example -->
+    <div id="todo-table-info"></div>
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <div class="form-group row p-0 my-0">
@@ -32,5 +32,6 @@
     @include('form-message')
 
     <script type="application/javascript">let access_token = "{{$access_token}}";</script>
+    <script type="text/html" id="csrf"> {{csrf_field()}} </script>
     <script src="/js/todo.js"></script>
 @stop
