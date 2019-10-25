161e88109a6c573322a05f26e0fec715cdf47fd8
Guilherme Mattioli <djmattioli@gmail.com>
2019-10-25 17:52:47 -0300

    Alterado modal para inclusão de tarefas para ser mais semelhante com a tabela exibida (e ser mais exclarecedor) + adicionada lógica para carregamento de tabela de tarefas, remoção de item individual, inclusão de novo item com o modal atualizado e paginação

---
+++ ToDoList/resources/js/todo.js
@@ -0,0 +1,129 @@
+$(document).ready(function () {
+
+    function close_all_msg() {
+        $('.alert-dismissible a.close').click();
+    }
+
+    function error_msg(msg = null) {
+        let div_msg = $($('#frm-msg-default').html());
+        if (msg) {
+            div_msg.find('.frm-msg').html(msg);
+        }
+        $('#todo-table-info').prepend(div_msg);
+    }
+
+    function success_msg(msg = null) {
+        close_all_msg();
+        let div_msg = $($('#frm-msg-success').html());
+        if (msg) {
+            div_msg.find('.frm-msg').html(msg);
+        }
+        $('#todo-table-info').prepend(div_msg);
+    }
+
+    function api_request(data, type = 'GET', noResponse = true, uri = 'task') {
+        return $.ajax({
+            type: type,
+            url: '/api/' + uri,
+            data: data,
+            dataType: (noResponse ? 'text' : 'json')
+        })
+    }
+
+    function default_fail_action(data) {
+        if (data.responseJSON.message !== 'Unauthorized') {
+            error_msg(data.responseJSON.message);
+        } else {
+            error_msg('Session expired.');
+        }
+    }
+
+    let curr_page = 1;
+    let load_table = function (json) {
+        if (!json.data.length && curr_page > 1) {
+            curr_page--;
+            return load_info();
+        }
+
+        let csrf = $($('#csrf').html());
+        let ajax_data = {
+            tasks: JSON.stringify(json)
+        };
+        ajax_data[csrf.attr('name')] = csrf.val();
+
+        $.ajax({
+            type: 'POST',
+            url: '/todo/table?access_token=' + access_token,
+            data: ajax_data,
+            dataType: 'html'
+        })
+            .done(function (data) {
+                $('#todo-table').html(data);
+            })
+            .fail(function (data) {
+                console.log(data);
+                let msg = $($('#frm-msg-default').html());
+                msg.find('.frm-msg').html('Session expired.');
+                $('#todo-table-info').prepend(msg);
+            });
+    };
+
+    let load_info = function (perpage = 10) {
+        api_request({
+            page: curr_page,
+            per_page: perpage,
+            access_token: access_token
+        }, 'GET', false)
+            .done(load_table)
+            .fail(default_fail_action);
+    };
+    load_info();
+
+    window.todo = {
+        load_info: load_info,
+        del: function (id) {
+            let task_name = $('[task=' + id + '] [name=name]').val();
+            if (confirm('Are you sure to delete te task: "' + task_name + '"?')) {
+                api_request({
+                    id: id,
+                    access_token: access_token
+                }, 'DELETE')
+                    .done(function (responseJSON) {
+                        success_msg('Task "' + task_name + '" deleted.');
+                        load_info();
+                    })
+                    .fail(default_fail_action);
+            }
+
+            return false;
+        },
+        add: function () {
+            let task_name = $('[task=new] [name=name]').val();
+            api_request({
+                name: task_name,
+                status: ($('[task=new] [name=status]')[0].checked ? 'done' : 'todo'),
+                access_token: access_token
+            }, 'POST')
+                .done(function (responseJSON) {
+                    success_msg('Task "' + task_name + '" added.');
+                    load_info();
+                    $('#addTaskModal').modal('hide');
+                    $('#frm-addTask').trigger("reset");
+                })
+                .fail(default_fail_action);
+            return false;
+        }
+    };
+
+    $(document).on('click', '.pagination a.page-link[rel="prev"]', function () {
+        curr_page--;
+        load_info();
+        return false;
+    });
+
+    $(document).on('click', '.pagination a.page-link[rel="next"]', function () {
+        curr_page++;
+        load_info();
+        return false;
+    });
+});
--- ToDoList/resources/views/private/modais/add-task.blade.php
+++ ToDoList/resources/views/private/modais/add-task.blade.php
@@ -4,28 +4,42 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Nova Tarefa:</h5>
+                <h5 class="modal-title" id="exampleModalLabel">New task</h5>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true"><i class="fas fa-times"></i></span>
                 </button>
             </div>
             <div class="modal-body">
+                <table class="table my-0" id="todo-table-add">
+                    <thead class="thead-blue">
+                    <tr>
+                        <th scope="col">Status</th>
+                        <th scope="col">Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td colspan="2" class="p-1" task="new">
+                            <form class="user" method="post" id="frm-addTask">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text" title="It's done?" data-toggle="tooltip"
+                                             data-placement="bottom">
+                                            <input type="checkbox" name="status">
+                                        </div>
+                                    </div>
+                                    <input type="text" name="name" class="form-control" value="">
+                                </div>
+                            </form>
+                        </td>
+                    </tr>
 
-                <form class="user" method="post" id="frm-addTask">
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">
-                                <input type="checkbox" aria-label="Checkbox for following text input">
-                            </div>
-                        </div>
-                        <input type="text" class="form-control" aria-label="Text input with checkbox">
-                    </div>
-                </form>
-
+                    </tbody>
+                </table>
             </div>
             <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                <a class="btn btn-primary" id="btn-addTask" href="#">Salvar</a>
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" id="btn-addTask" href="#" onclick="return window.todo.add();">Save</a>
             </div>
         </div>
     </div>
